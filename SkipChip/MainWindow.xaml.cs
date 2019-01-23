e45a19a67ad09aead20341b6d5735b101e8af555
Zxited <mmgamesfull@gmail.com>
2019-01-23 13:11:26 +0100

    Meget

--- SkipChip/MainWindow.xaml.cs
+++ SkipChip/MainWindow.xaml.cs
@@ -20,14 +20,117 @@ namespace SkipChip
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Person> persons = new List<Person>();
+        private Person currentPerson = new Person("person-icon.png", "no", 0, new List<string>());
+        private int personIndex = 0;
         public MainWindow()
         {
             InitializeComponent();
+            List<string> brianOffenses = new List<string>();
+            brianOffenses.Add("(1/1 - 2018: Slog en mand ned på Torvet");
+            List<string> marianneOffenses = new List<string>();
+            List<string> hanneOffenses = new List<string>();
+
+            persons.Add(new Person("brian.jpg", "Brian", 21, brianOffenses));
+            persons.Add(new Person("marianne.jpg", "Marianne", 69, marianneOffenses));
+            persons.Add(new Person("hanne.jpg", "Hanne", 31, hanneOffenses));
+            KeyDown += new KeyEventHandler(SimulateScanIn);
         }
 
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void SimulateScanIn(object sender, KeyEventArgs e)
         {
+            if (e.Key == Key.F1 && persons.Count > personIndex)
+            {
+                ListBox.Items.Clear();
+                ScanIn(persons[personIndex]);
+                personIndex++;
+            }
+        }
 
+        void ScanIn(Person p)
+        {
+            ProfilePic.Source = p.ProfilePic;
+            NameAge.Content = p.Name + ", " + p.Age;
+            if (p.offenses.Count > 0)
+            {
+                foreach (string offense in p.offenses)
+                {
+                    ListBox.Items.Add(offense);
+                }
+            }
+            else
+            {
+                ListBox.Items.Add("Ingen bemærkninger");
+            }
+            StackPanel newPanel = new StackPanel();
+            Label name = new Label();
+            DateTime date = DateTime.Today;
+            Label dateLabel = new Label();
+            dateLabel.Content = date;
+            Image profileImage = new Image();
+            profileImage.Height = 50;
+            profileImage.Source = p.ProfilePic;
+            name.Content = p.Name;
+            newPanel.Children.Add(dateLabel);
+            newPanel.Children.Add(name);
+            History.Items.Add(newPanel);
+        }
+
+        private void btnFindBruger_Click(object sender, RoutedEventArgs e)
+        {
+            Person p = findPerson(tbBruger.Text);
+            if (p != null)
+            {
+                currentPerson = p;
+                ProfilePicFind.Source = p.ProfilePic;
+                NameAgeReg.Content = p.Name + ", " + p.Age;
+                DatePicker.IsEnabled = true;
+                Offense.IsEnabled = true;
+            }
+            else
+            {
+                tbBruger.Text = "Kunne ikke finde personen.";
+                ProfilePic.Source = null;
+                NameAgeReg.Content = "Name, Alder";
+            }
+        }
+
+        Person findPerson(string name)
+        {
+            if (name != "")
+            {
+                foreach (var person in persons)
+                {
+                    if (person.Name.Contains(name))
+                    {
+                        return person;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void AddOffense_Click(object sender, RoutedEventArgs e)
+        {
+            if (DatePicker.SelectedDate != null  && Offense.Text != "")
+            {
+                var date = DatePicker.SelectedDate.Value;
+                currentPerson.offenses.Add("(" + date.Day + "/" + date.Month + " - " + date.Year + ") " + Offense.Text);
+                DatePicker.SelectedDate = null;
+                Offense.Text = string.Empty;
+                AddOffense.IsEnabled = false;
+                Message.Content = "Bemærkning Tilføjet!";
+            }
+            else
+            {
+                Message.Content = "Husk alle felter!";
+            }
+            
+        }
+
+        private void Offense_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AddOffense.IsEnabled = true;
         }
     }
 }
---
+++ SkipChip/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SkipChip
+{
+    public class Person
+    {
+        public List<string> offenses = new List<string>();
+        public BitmapImage ProfilePic;
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string profilePic, string name, int age, List<string> offenses)
+        {
+            ProfilePic = new BitmapImage(new Uri(Path.Combine(Environment.CurrentDirectory, profilePic)));
+            Name = name;
+            Age = age;
+            this.offenses = offenses;
+        }
+    }
+}
